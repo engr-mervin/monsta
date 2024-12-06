b9a4678d125a8ada27eb03e686d321f99403308c
John Mervin Bocatcat <johnmervin.bocatcat@888holdings.com>
2024-12-07 02:01:29 +0800

    Added setup

--- src/classes/MondayClient.ts
+++ src/classes/MondayClient.ts
@@ -1,8 +1,11 @@
 import { getBoard } from "../queries/methods/getBoard";
+import { getBoards } from "../queries/methods/getBoards";
 import { getGroup } from "../queries/methods/getGroup";
+import { getGroups } from "../queries/methods/getGroups";
 import { getItem } from "../queries/methods/getItem";
 import { getItems } from "../queries/methods/getItems";
 import { getUser } from "../queries/methods/getUser";
+import { getUsers } from "../queries/methods/getUsers";
 import { getWorkspace } from "../queries/methods/getWorkspace";
 import {
   Group_RowQuery,
@@ -10,7 +13,6 @@ import {
   QueryRequestOptions,
   QueryLevel,
   Item_CellQuery,
-  Items_CellQuery,
   RequestOptions,
 } from "../types/types";
 
@@ -30,22 +32,44 @@ export class MondayClient {
   /** You should use this if you need the whole board / groups / items, if you know you'll only need specific entities, then use specific query */
   public async getWorkspace(
     workspaceId: string | number,
-    requestOptions: QueryRequestOptions = { queryLevel: QueryLevel.Board }
+    requestOptions: QueryRequestOptions & { includeColumns: boolean } = {
+      queryLevel: QueryLevel.Board,
+      includeColumns: false,
+    }
   ) {
     return await getWorkspace(this.clientOptions, workspaceId, requestOptions);
   }
 
   public async getBoard(
     boardId: string | number,
-    requestOptions: QueryRequestOptions = { queryLevel: QueryLevel.Board }
+    requestOptions: QueryRequestOptions & { includeColumns: boolean } = {
+      queryLevel: QueryLevel.Board,
+      includeColumns: false,
+    }
   ) {
     return await getBoard(this.clientOptions, boardId, requestOptions);
+  }
+
+  public async getBoards(
+    boardIds: (string | number)[],
+    requestOptions: QueryRequestOptions & { includeColumns: boolean } = {
+      queryLevel: QueryLevel.Board,
+      includeColumns: false,
+    }
+  ) {
+    return await getBoards(this.clientOptions, boardIds, requestOptions);
   }
   public async getGroup(
     group: Group_RowQuery,
     requestOptions: QueryRequestOptions = { queryLevel: QueryLevel.Group }
   ) {
     return await getGroup(this.clientOptions, group, requestOptions);
+  }
+  public async getGroups(
+    groups: { boardId: string | number; groupIds: string[] },
+    requestOptions: QueryRequestOptions = { queryLevel: QueryLevel.Group }
+  ) {
+    return await getGroups(this.clientOptions, groups, requestOptions);
   }
 
   public async getItem(
@@ -59,7 +83,7 @@ export class MondayClient {
   }
 
   public async getItems(
-    items: Items_CellQuery,
+    items: (string | number)[],
     requestOptions: QueryRequestOptions = {
       queryLevel: QueryLevel.Item,
       subitemLevel: "none",
@@ -68,18 +92,18 @@ export class MondayClient {
     return await getItems(this.clientOptions, items, requestOptions);
   }
 
-
   public async getUser(
     userId: string | number,
     requestOptions: RequestOptions = {}
-  ){
+  ) {
     return await getUser(this.clientOptions, userId, requestOptions);
   }
 
-  // public async getSubitemsByItem(
-  //   item: Item_SubitemQuery,
-  //   requestOptions: QueryRequestOptions = { queryLevel: QueryLevel.Item }
-  // ) {
-  //   return await getSubitemsByItem(this.clientOptions, item, requestOptions);
-  // }
+  public async getUsers(
+    userIds: (string | number)[],
+    requestOptions: RequestOptions = {}
+  ) {
+    return await getUsers(this.clientOptions, userIds, requestOptions);
+  }
+
 }
